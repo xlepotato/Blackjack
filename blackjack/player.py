8eee178c93c337da9ea4bd637b2d50df0e2665c6
xlepotato <yucia181@gmail.com>
2024-02-08 17:51:31 +0800

    feat(blackjack): add Player class

---
+++ blackjack/player.py
@@ -0,0 +1,11 @@
+from baseplayer import BasePlayer
+
+class Player(BasePlayer):
+    def __init__(self, balance):
+        super().__init__()
+        self.balance = balance
+
+
+    def __repr__(self):
+        return (f"{self.__class__.__name__}("
+                f"{self.balance!r})")
