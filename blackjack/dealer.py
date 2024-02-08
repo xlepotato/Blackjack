425141ea2b679fe9b27026ddf36bcbe805256441
xlepotato <yucia181@gmail.com>
2024-02-08 17:51:57 +0800

    feat(blackjack): add Dealer class

---
+++ blackjack/dealer.py
@@ -0,0 +1,9 @@
+from baseplayer import BasePlayer
+
+class Dealer(BasePlayer):
+    def __init__(self):
+        super().__init__()
+
+
+    def __repr__(self):
+        return (f"{self.__class__.__name__}()")
